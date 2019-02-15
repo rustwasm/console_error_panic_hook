de6b3a9007a81b785d857b196dcd993fe73eeea3
Nick Fitzgerald <fitzgen@gmail.com>
2019-02-15 10:19:25 -0800

    Include stacks in logged messages; fix misbehaving browser devtools
    T.T
    
    Fixes #7

--- src/lib.rs
+++ src/lib.rs
@@ -79,10 +79,42 @@ cfg_if! {
         extern {
             #[wasm_bindgen(js_namespace = console)]
             fn error(msg: String);
+
+            type Error;
+
+            #[wasm_bindgen(constructor)]
+            fn new() -> Error;
+
+            #[wasm_bindgen(structural, method, getter)]
+            fn stack(error: &Error) -> String;
         }
 
         fn hook_impl(info: &panic::PanicInfo) {
-            error(info.to_string());
+            let mut msg = info.to_string();
+
+            // Add the error stack to our message.
+            //
+            // This ensures that even if the `console` implementation doesn't
+            // include stacks for `console.error`, the stack is still available
+            // for the user. Additionally, Firefox's console tries to clean up
+            // stack traces, and ruins Rust symbols in the process
+            // (https://bugzilla.mozilla.org/show_bug.cgi?id=1519569) but since
+            // it only touches the logged message's associated stack, and not
+            // the message's contents, by including the stack in the message
+            // contents we make sure it is available to the user.
+            msg.push_str("\n\nStack:\n\n");
+            let e = Error::new();
+            let stack = e.stack();
+            msg.push_str(&stack);
+
+            // Safari's devtools, on the other hand, _do_ mess with logged
+            // messages' contents, so we attempt to break their heuristics for
+            // doing that by appending some whitespace.
+            // https://github.com/rustwasm/console_error_panic_hook/issues/7
+            msg.push_str("\n\n");
+
+            // Finally, log the panic with `console.error`!
+            error(msg);
         }
     } else {
         use std::io::{self, Write};
